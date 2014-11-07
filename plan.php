23c598067ef59e92a182cd25df5f22dd58b3f88a
tcomte <comte.thibaut@gmail.com>
2014-11-07 14:54:44 +0100

    Update plan layout

--- plan.php
+++ plan.php
@@ -1,17 +1,18 @@
 <?php $titre = "Plan d'évacuation" ?>
 
 <?php ob_start(); ?>
-<h1><b><Center>Plan d'évacuation</Center></b></h1>
-<div class="row">
-	<div class="col-xs-6 col-md-4 col-xs-offset-3 col-md-offset-4">
-		<a href="#" class="thumbnail">
-			<img src="./img/plan.jpg" alt="...">
-		</a>
-	</div>
-
+<div class="row text-center">
+  <h1>Plan d'évacuation</h1>
 </div>
 
-<button type="button" class="btn btn-default" onclick="self.location.href='index.php'">Retour</button> </br></br>
+<div class="row">
+  <div class="col-xs-6 col-xs-offset-3">
+    <img src="./img/plan.jpg" alt="..." class="img-responsive">
+    <a class="btn btn-lg btn-default pull-right" href="index.php">
+			Retour
+		</a>
+  </div>
+</div>
 <?php $contenu = ob_get_clean(); ?>
 
 <?php require 'layout.php'; ?>
