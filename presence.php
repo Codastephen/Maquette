ccd9bef03e06b1f1df85922aa413fc7e728291c5
tcomte <comte.thibaut@gmail.com>
2014-11-07 14:23:18 +0100

    MAJ du layout presence.php

--- presence.php
+++ presence.php
@@ -1,9 +1,34 @@
 <?php $titre = "Présence" ?>
 
 <?php ob_start(); ?>
-<button type="button" class="btn btn-default" onclick="self.location.href='formulairePresence.php'"  >Signaler ma présence</button> </br></br>
-	<button type="button" class="btn btn-default" onclick="self.location.href='connecte.php'">J'ai déjà signalé ma présence</button> </br></br>
-	<button type="button" class="btn btn-default" onclick="self.location.href='index.php'">Retour</button> </br></br>
+<div class="row" style="margin-bottom : 50px">
+	<div class="col-lg-4 col-lg-offset-4">
+		<img src="./img/designal.png" class="img-responsive"/>
+	</div>
+</div>
+<div class="row">
+	<div class="col-sm-6 col-sm-offset-3">
+		<div class="row">
+			<div class="col-sm-5">
+				<a class="btn btn-lg btn-success" href="formulairePresence.php">
+					Signaler ma présence
+				</a>
+			</div>
+			<div class="col-sm-5 col-sm-offset-2">
+				<a class="btn btn-lg btn-danger" href="connecte.php">
+					J'ai déjà signalé ma présence
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="row" style="margin-top : 5%">
+	<div class="col-sm-6 col-sm-offset-3 text-center">
+		<a class="btn btn-lg btn-default" href="index.php">
+			Retour
+		</a>
+	</div>
+</div>
 <?php $contenu = ob_get_clean(); ?>
 
 <?php require 'layout.php'; ?>
