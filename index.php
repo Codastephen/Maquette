435d5ec664dd85f97e0d9fab5380335c2fdb9e3f
tcomte <comte.thibaut@gmail.com>
2014-11-07 11:06:46 +0100

    update

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 
 		<div class="col-lg-4 col-lg-offset-4">
 			<div class="col-lg-6">
-				<button type="button" class="btn btn-default" onclick="self.location.href='presence.php'"  >Signaler votre présence</button> </br></br>
+				<button type="button" class="btn btn-default" onclick="self.location.href='presence.php'"  >Signaler votre présence2222</button> </br></br>
 				<button type="button" class="btn btn-default" onclick="self.location.href='plan.php'">Obtenir le plan</button> </br></br>
 
 				<button type="button" class="btn btn-default" onclick="self.location.href='depart.php'">Signaler votre départ</button> </br></br>
