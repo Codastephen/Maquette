cb706f8fab27e33c25d807b70ee10feb3d08f103
Codastephen <coda.stephen@gmail.com>
2014-11-07 11:31:13 +0100

    delete personnage

--- client.php
+++ client.php
@@ -1,12 +1,16 @@
 <?php
 class Client
 {
-
-        
+	private $_nom;
+	private $_prenom;
+    private $_societe;
+	private $_hArrive;
+	private $_hDepart;
+	
   // Nous déclarons une méthode dont le seul but est d'afficher un texte.
-  public function test()
+  public function toString()
   {
-
+	echo'$_nom' . '$_prenom'. '$_societe' . '$_nom' . '$_nom';
   }
 }
 ?>
