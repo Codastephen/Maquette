b9c065da8a7790f16eddc3a951c57deff515bc03
tcomte <comte.thibaut@gmail.com>
2014-11-07 14:08:33 +0100

    Update layout for index file

--- index.php
+++ index.php
@@ -3,25 +3,37 @@
 <?php ob_start(); ?>
 <div class="row" style="margin-bottom : 50px">
 	<div class="col-lg-4 col-lg-offset-4">
-		<img src="./img/designal.jpg"/>
+		<img src="./img/designal.png" class="img-responsive"/>
 	</div>
 </div>
 <div class="row">
 	<div class="col-sm-6 col-sm-offset-3">
 		<div class="row">
 			<div class="col-sm-5">
-				<button type="button" class="btn btn-lg btn-success btn-block" onclick="self.location.href='presence.php'"  >Signaler votre présence</button> </br></br>
+				<a href="presence.php">
+					<img src="./img/arrivee.png" class="img-responsive"/>
+				</a>
 			</div>
 			<div class="col-sm-5 col-sm-offset-2">
-				<button type="button" class="btn btn-lg btn-danger btn-block" onclick="self.location.href='depart.php'">Signaler votre départ</button> </br></br>
-			</div>	
+				<a href="depart.php">
+					<img src="./img/depart.png" class="img-responsive"/>
+				</a>
+			</div>
 		</div>
+	</div>
+</div>
+<div class="row" style="margin-top : 20px">
+	<div class="col-sm-6 col-sm-offset-3">
 		<div class="row">
 			<div class="col-sm-5">
-				<button type="button" class="btn btn-lg btn-primary btn-block" onclick="self.location.href='plan.php'">Obtenir le plan</button> </br></br>
-			</div>	
+				<a href="plan.php">
+					<img src="./img/map.png" class="img-responsive"/>
+				</a>
+			</div>
 			<div class="col-sm-5 col-sm-offset-2">
-				<button type="button" class="btn btn-lg btn-info btn-block">Aide </button> </br></br>
+				<a href="help.php">
+					<img src="./img/help.png" class="img-responsive"/>
+				</a>
 			</div>	
 		</div>
 	</div>
