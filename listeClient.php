3574e529e934c98d3f1e6f125bea3d8d7e615d37
tcomte <comte.thibaut@gmail.com>
2014-11-12 10:55:23 +0100

    reindent files

--- listeClient.php
+++ listeClient.php
@@ -3,34 +3,29 @@ class ListeClient
 {
 	var $_liste;
 
-
 	function __construct(){
-        $this->_liste = array();
-    }
+    $this->_liste = array();
+  }
 
-
-    public function ajouter($client){
-       $this->_liste[] = $client;
-       $last_key = key( array_slice( $this->_liste, -1, 1, TRUE ) );
-       return $last_key; //output "last"
+  public function ajouter($client){
+    $this->_liste[] = $client;
+    $last_key = key( array_slice( $this->_liste, -1, 1, TRUE ) );
+    return $last_key;
+  }
+  public function supprimer($index){
+    unset($this->_liste[$index]);
+    array_merge($this->_liste );
+  }
+  public function size(){
+    return sizeof($this->_liste);
+  }
+  public function toString(){
+    foreach ($this->_liste as $item) {
+      echo $item->toString();
     }
-
-    public function supprimer($index){
-        unset($this->_liste[$index]);
-        array_merge($this->_liste );
-    }
-    public function size(){
-        return sizeof($this->_liste);
-    }
-
-    public function toString(){
-        foreach ($this->_liste as $item) {
-            echo $item->toString();
-        }
-    }
-
-    public function getItem($index){
-        return $this->_liste[$index];
-    }
+  }
+  public function getItem($index){
+    return $this->_liste[$index];
+  }
 }
 ?>
