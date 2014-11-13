4aa45016a14b2f4e93795439ffb0a92e039585a3
Codastephen <coda.stephen@gmail.com>
2014-11-13 11:14:26 +0100

    Liste

--- formulairePresence.php
+++ formulairePresence.php
@@ -27,9 +27,13 @@ require_once 'autoload.php';
       </th>
       <th></th>
       <?php
-      foreach ($_SESSION['liste']->_liste as $key=>$item) {
-        echo "<tr> <td> ".$item->_nomprenom." </td> <td><a href='listeContact.php?id=".$key."' class='btn btn-primary' style='opacity:0'>C'est bien moi</a></td><tr>";
-      }
+			$conn = new connexionBDD();
+			$reponse = $conn->afficherClient();
+			
+			while ($donnees = $reponse->fetch())
+			{
+				echo "<tr> <td> ".$donnees['Nom']." </td><tr>";
+			}		
       ?>
     </table>
   </div>
