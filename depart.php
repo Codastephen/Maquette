b309902d03a3a90a66b9d41e06d444fda52dbdd2
Codastephen <coda.stephen@gmail.com>
2014-11-13 11:49:25 +0100

    Correction

---
+++ depart.php
@@ -0,0 +1,33 @@
+<?php 
+require_once("autoload.php");
+?>
+
+<div class="row">
+	<div class="col-xs-8 col-xs-offset-2">
+		<h1 class="text-center">Qui êtes-vous?</h1>
+		<table id="tablevisitor" class="table table-hover table-striped">
+			<thead>
+				<tr>
+					<th col-width=4>
+						Nom et prénom
+					</th>
+					<th col-width=2></th>
+				</tr>
+			</thead>
+			<tbody>
+			<?php
+			
+			$conn = new connexionBDD();
+			$reponse = $conn->afficherClient();
+			
+			while ($donnees = $reponse->fetch())
+			{
+				echo "<tr> <td> ".$donnees['Nom']." </td> <td><button class='btn btn-success' style='opacity:0' onclick='$(this).next().show();$(this).fadeOut();'>Me déconnecter</button><a class='btn btn-danger btn-hidden' onclick='validate(".$key.")' style='vertical-align:middle'>Valider</a><td><tr>";
+			}
+				
+			?>
+			</tbody>
+		</table>
+
+	</div>
+</div>
