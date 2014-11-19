c2675beefeb1d04c4a7a70cade31657dbd708b6f
tcomte <comte.thibaut@gmail.com>
2014-11-19 16:07:13 +0100

    update for beter accuravy

--- getMsg.php
+++ getMsg.php
@@ -5,11 +5,14 @@ session_start();
 $bdd = new connexionBDD();
 $reponse = $bdd->getAllMsg();
 $msg="<p>";
+$first=true;
 while ($donnees = $reponse->fetch())
 {
-	if(date('Y-m-d H:i')<$donnees['datedebut'] || date('Y-m-d H:i')>$donnees['datefin'] )
+	if(date('Y-m-d H:i:s')<$donnees['datedebut'] || date('Y-m-d H:i:s')>$donnees['datefin'] )
 		continue;
 
+	if(!$first)
+		$msg.=" - ";
 	$msg .= $donnees['message'];
 	$first = false;
 }
