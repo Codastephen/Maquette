856c7917ea25c76b88ca20a042ce0440f3f211d6
Thibaut Comte <tcomte@designalsystems.com>
2014-12-03 15:53:24 +0100

    Update evac plan

--- plan.php
+++ plan.php
@@ -1,9 +1,5 @@
-<div class="row text-center">
-  <h1>Plan d'évacuation</h1>
-</div>
-
 <div class="row">
   <div class="col-xs-6 col-xs-offset-3">
-    <img src="./img/plan.jpg" alt="..." class="img-responsive" style="height:200px">
+    <img src="./img/evac.png" alt="..." class="img-responsive" style="height:400px">
   </div>
 </div>
