55e3bddb18258c378c2afdfd0144137c1b858c27
Thibaut Comte <tcomte@designalsystems.com>
2014-12-05 11:07:14 +0100

    improved timer marquee

--- js/timer.js
+++ js/timer.js
@@ -2,29 +2,12 @@ $(document).ready(function(){
 	var index = window.location.pathname.lastIndexOf("/") + 1;
 	var filename = window.location.pathname.substr(index);
 
-	if(filename == 'index.php'){
-		repeatEvery(getMsg, 60 * 1000);
+	if(filename == 'index.php' || filename == ''){
+		getMsg();
 	}
 	
 
 });
-
-function repeatEvery(func, interval) {
-    // Check current time and calculate the delay until next interval
-    var now = new Date,
-        delay = interval - now % interval;
-
-    function start() {
-        // Execute function now...
-        func();
-        // ... and every interval
-        setInterval(func, interval);
-    }
-
-    getMsg();
-    // Delay execution until it's an even interval
-    setTimeout(start, delay);
-}
 
 function getMsg()
 {
@@ -40,5 +23,21 @@ function getMsg()
 		} 
 	});
 	$("#defilor").html(result);
-	$('#defilor').marquee();
+	$('#defilor')
+	.bind('finished', function () {
+		var result = null;
+		var scriptUrl = "getMsg.php";
+		$.ajax({
+			url: scriptUrl,
+			type: 'get',
+			dataType: 'html',
+			async: false,
+			success: function(data) {
+				result = data;
+			} 
+		});
+		$("#defilor").html(result);
+		$("#defilor").marquee();
+	})
+	.marquee()
 }
